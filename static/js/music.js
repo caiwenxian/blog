30bd04ca1f7aa34feadb21334b956f5ee9c55e67
wenxian.cai <1271970096@qq.com>
2017-09-30 13:55:11 +0800

    commit idea

---
+++ static/js/music.js
@@ -0,0 +1,18 @@
+/**
+ * music模块 js
+ */
+
+ var music = {};
+
+ music.init = function () {
+
+ }
+ music.vm = new Vue({
+    el: '.vue-content',
+    data: {
+
+    },
+    methods: {
+
+    }
+ });
